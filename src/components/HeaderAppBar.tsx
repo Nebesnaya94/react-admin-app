b4e0cd495956b0f3ddeb30ad20b258ab26e4c0c8
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-11-03 18:58:34 +0300

    feat: edit header, move logout to modal

--- src/components/HeaderAppBar.tsx
+++ src/components/HeaderAppBar.tsx
@@ -1,20 +1,34 @@
 import React, { FC } from "react";
-import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
+import LogoutIcon from "../assets/images/icon_logout.svg";
+import { LogoutModal } from "./UI/LogoutModal";
 
 export const HeaderAppBar: FC = () => {
+  const [showModal, setShowModal] = React.useState<boolean>(false);
+
+  const handleShowModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <AppBar className="header">
       <Toolbar className="header__wrapper">
         <h1 className="header__logo">LOGO</h1>
-        <Link to="/profile">
-          <Button variant="text" className="header__profile">
-            KM
-          </Button>
-        </Link>
+        <Button
+          onClick={() => setShowModal(true)}
+          variant="text"
+          className="header__logout-button"
+        >
+          <img
+            src={LogoutIcon}
+            alt="logout-icon"
+            className="header__logout-icon"
+          />
+        </Button>
       </Toolbar>
+      <LogoutModal show={showModal} handleShow={handleShowModal} />
     </AppBar>
   );
 };
