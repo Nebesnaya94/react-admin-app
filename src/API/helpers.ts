8a5983cc679c3a2cd6851dc1327f10e90e412dd2
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-11-08 00:17:27 +0300

    feat: complete typing

--- src/API/helpers.ts
+++ src/API/helpers.ts
@@ -1,6 +1,6 @@
 import { API_URL, httpClient } from "./dataProvider";
 
-export async function fetchUser(userId) {
+export async function fetchUser(userId: number) {
   try {
     const { json } = await httpClient(`${API_URL}/users/${userId}`);
     return json;
@@ -10,7 +10,7 @@ export async function fetchUser(userId) {
   }
 }
 
-export async function fetchFilteredData(filter) {
+export async function fetchFilteredData(filter: string) {
   try {
     const { json } = await httpClient(`${API_URL}/${filter}`);
     return json;
@@ -20,6 +20,6 @@ export async function fetchFilteredData(filter) {
   }
 }
 
-export function capitalize(str) {
+export function capitalize(str?: string) {
   return str ? str[0].toUpperCase() + str.slice(1) : "";
 }
